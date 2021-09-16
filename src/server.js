05b14da5ec9d8ade5dbe00d43f5f3227b60c122d
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-09-16 09:40:18 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import expess from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from './route/web';
+require('dotenv').config();
+
+let app = expess();
+
+//config app
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}))
+
+viewEngine(app);
+initWebRoutes(app);
+
+let port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log("Backend Nodejs is runing on the port : " + port)
+})
