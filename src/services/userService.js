28c5140fba2fef07154d1a615822499af586c0e5
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-09-23 14:16:53 +0700

    #8 LoginAPI

---
+++ src/services/userService.js
@@ -0,0 +1,66 @@
+import db from "../models/index";
+import bcrypt  from 'bcryptjs';
+import e from "express";
+
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+
+            let isExist = await checkUserEmail(email);
+            if(isExist) {
+
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email : email},
+                    raw: true
+                });
+                if(user){
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if(check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'ok';
+                        delete user.password;
+                        userData.user = user;
+                    }else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password';
+                    }
+                }else {
+                    userData.errCode = 2;
+                    userData.errMessage = 'User is not found'
+                }
+                
+            }else {
+                userData.errCode = 1;
+                userData.errMessage = 'Your is Email is not exist in system. Please try other Email!'
+            }
+
+            resolve(userData)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email : userEmail}
+            })
+            if(user) {
+                resolve(true)
+            }else{
+                resolve(false)
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+module.exports = {
+    handleUserLogin: handleUserLogin
+}
