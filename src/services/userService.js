4d1cf00fefd1bb3b2efe3da9f5675cb83336694e
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-09-29 14:28:14 +0700

    #12 FixAPICreateNewUser

--- src/services/userService.js
+++ src/services/userService.js
@@ -107,25 +107,25 @@ let createNewUser = (data) => {
             if(check === true){
                 resolve({
                     errCode: 1,
-                    message: 'Your email is exist, Please try again!'
+                    errMessage: 'Your email is exist, Please try again!'
+                })
+            }else{
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId
+                })
+                resolve({
+                    errCode: 0,
+                    message: 'ok'
                 })
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId
-            })
-
-            resolve({
-                errCode: 0,
-                message: 'ok'
-            })
         } catch (e) {
             reject(e);
         }
