297fa70040b49ba776c780a15d97958f14229743
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-10-23 17:21:02 +0700

    #14 TimeOut

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -63,8 +63,10 @@ let handleEditUser = async (req, res) => {
 
 let getAllCode = async (req, res) => {
     try {
-        let data = await userService.getAllCodeService(req.query.type);
-        return res.status(200).json(data);
+        setTimeout(async() => {
+            let data = await userService.getAllCodeService(req.query.type);
+            return res.status(200).json(data);
+        }, 1000)
     } catch (e) {
         console.log('Get all code error: ', e)
         return res.status(200).json({
