ef30d2f4b2b8384c5d292ba6f8109487753077b5
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-10-30 15:02:33 +0700

    #20 SaveInfoDr

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -10,11 +10,39 @@ let getTopDoctorHome = async (req, res) => {
         console.log(e);
         return res.status(200).json({
             errCode: -1,
-            message: 'Error from server...'
+            errMessage: 'Error from server...'
+        })
+    }
+}
+
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server...'
+        })
+    }
+}
+
+let postInfoDoctor = async (req, res) => {
+    try {
+        let response = await doctorService.saveDetailInfoDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server...'
         })
     }
 }
 
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    postInfoDoctor: postInfoDoctor
 }
