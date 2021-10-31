1eb342d63605e79b97216319f7604327ca94d616
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-10-31 19:01:59 +0700

    #21 APIDetailDr

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -41,8 +41,22 @@ let postInfoDoctor = async (req, res) => {
     }
 }
 
+let getDetailDoctorById = async (req, res) => {
+    try {
+        let info = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(info);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server...'
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
-    postInfoDoctor: postInfoDoctor
+    postInfoDoctor: postInfoDoctor, 
+    getDetailDoctorById: getDetailDoctorById
 }
--- src/route/web.js
+++ src/route/web.js
@@ -9,11 +9,9 @@ let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
     router.get('/crud', homeController.getCRUD);
-
     router.post('/post-crud', homeController.postCRUD);
     router.get('/get-crud', homeController.displayCRUD);
     router.get('/edit-crud', homeController.getEditCRUD);
-    
     router.post('/put-crud', homeController.putCRUD);
     router.get('/delete-crud', homeController.deleteCRUD);
 
@@ -22,13 +20,12 @@ let initWebRoutes = (app) => {
     router.post('/api/create-new-user', userController.handleCreateNewUser);
     router.put('/api/edit-user', userController.handleEditUser);
     router.delete('/api/delete-user', userController.handleDeleteUser);
-
     router.get('/api/allcode', userController.getAllCode);
     
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
-
     router.get('/api/get-all-doctors', doctorController.getAllDoctors);
     router.post('/api/save-info-doctors', doctorController.postInfoDoctor);
+    router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById);
 
     return app.use("/", router);
 }
