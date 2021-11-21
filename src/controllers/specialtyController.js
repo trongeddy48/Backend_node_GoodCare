520621e1926bb3c54afb8e02024470f7f5521101
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-11-21 14:41:36 +0700

    #36 AddSpec

---
+++ src/controllers/specialtyController.js
@@ -0,0 +1,18 @@
+import specialtyService from '../services/specialtyService';
+
+let createSpecialty = async (req, res) => {
+    try {
+        let info = await specialtyService.createSpecialty(req.body);
+        return res.status(200).json(info);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server...'
+        })
+    }
+}
+
+module.exports = {
+    createSpecialty: createSpecialty
+}
